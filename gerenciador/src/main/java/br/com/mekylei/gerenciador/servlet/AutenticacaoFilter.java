188dec123444ee7c5345dbf4ca6816ea3db8f20a
Mekylei-Belchior <mekylei@msn.com>
2021-07-05 18:59:25 -0300

    Inserido os métodos (init e destroy) nos filters

--- gerenciador/src/main/java/br/com/mekylei/gerenciador/servlet/AutenticacaoFilter.java
+++ gerenciador/src/main/java/br/com/mekylei/gerenciador/servlet/AutenticacaoFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -12,6 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class AutenticacaoFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
 
 	public void doFilter(ServletRequest requestServlet, ServletResponse responseServlet, FilterChain chain)
 			throws IOException, ServletException {

35c05bf2f9bfe74e59d15102ae06ff965a274b59
Mekylei-Belchior <mekylei@msn.com>
2021-07-12 12:48:49 -0300

    Spring MVC com Thymeleaf e Bootstrap - parte 1

---
+++ mudi/src/main/java/br/com/mekylei/mvc/mudi/model/Pedido.java
@@ -0,0 +1,95 @@
+package br.com.mekylei.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nomeProduto;
+	private BigDecimal valorNegociado;
+	private LocalDate dataDaEntrega;
+
+	@Lob
+	private String urlProduto;
+
+	@Lob
+	private String urlImagem;
+
+	@Lob
+	private String descricao;
+
+	@Enumerated(EnumType.STRING)
+	private StatusPedido statusPedido;
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public BigDecimal getValorNegociado() {
+		return valorNegociado;
+	}
+
+	public void setValorNegociado(BigDecimal valorNegociado) {
+		this.valorNegociado = valorNegociado;
+	}
+
+	public LocalDate getDataDaEntrega() {
+		return dataDaEntrega;
+	}
+
+	public void setDataDaEntrega(LocalDate dataDaEntrega) {
+		this.dataDaEntrega = dataDaEntrega;
+	}
+
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public StatusPedido getStatusPedido() {
+		return statusPedido;
+	}
+
+	public void setStatusPedido(StatusPedido statusPedido) {
+		this.statusPedido = statusPedido;
+	}
+
+}
---
+++ mudi/src/main/java/br/com/mekylei/mvc/mudi/model/StatusPedido.java
@@ -0,0 +1,6 @@
+package br.com.mekylei.mvc.mudi.model;
+
+public enum StatusPedido {
+	AGUARDANDO, APROVADO, ENTREGUE;
+
+}

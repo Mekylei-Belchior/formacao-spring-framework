eaffbdf275ca2ab84f72927db14cfb99747b2fc6
Mekylei-Belchior <mekylei@msn.com>
2021-07-19 13:43:37 -0300

    Spring Boot API REST: restringindo acesso a determinados endpoints por perfil

--- spring-boot-api-rest-forum/src/main/java/br/com/mekylei/forum/config/security/SecurityConfigurations.java
+++ spring-boot-api-rest-forum/src/main/java/br/com/mekylei/forum/config/security/SecurityConfigurations.java
@@ -50,6 +50,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 				.antMatchers(HttpMethod.POST, "/auth").permitAll() // reajustar para utilizar em produção
 				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+				.antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
 				.anyRequest().authenticated()
 				.and().csrf().disable()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- spring-boot-api-rest-forum/src/main/resources/data.sql
+++ spring-boot-api-rest-forum/src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIOS(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$nn3k0y5kHyTHwdbCMKC9uu2sajSMoQxirOosN8.0tX3YTW3AVz5gC');
+INSERT INTO USUARIOS(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$nn3k0y5kHyTHwdbCMKC9uu2sajSMoQxirOosN8.0tX3YTW3AVz5gC');
+
+INSERT INTO PERFIS(id, nome) VALUES(1, 'ROLE_ALUNO');
+INSERT INTO PERFIS(id, nome) VALUES(2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIOS_PERFIS(usuario_id, perfis_id) VALUES(1, 1);
+INSERT INTO USUARIOS_PERFIS(usuario_id, perfis_id) VALUES(2, 2);
 
 INSERT INTO CURSOS(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSOS(nome, categoria) VALUES('HTML 5', 'Front-end');

12d7b6c9da209efb90bddad176d7b342a62ae2ba
Mekylei-Belchior <mekylei@msn.com>
2021-07-20 23:07:57 -0300

    Spring Boot API REST: deploy com Docker e Heroku

--- spring-boot-api-rest-forum/src/main/java/br/com/mekylei/forum/ForumApplication.java
+++ spring-boot-api-rest-forum/src/main/java/br/com/mekylei/forum/ForumApplication.java
@@ -2,21 +2,14 @@ package br.com.mekylei.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
-public class ForumApplication extends SpringBootServletInitializer {
+public class ForumApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ForumApplication.class);
 	}
 
 }
